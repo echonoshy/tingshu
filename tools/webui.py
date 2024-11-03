bd008a2d619a94d6b2a1bd23e78cbc3dd71bef98
Lake <echonoshy@gmail.com>
2024-11-03 22:51:11 +0800

    fix: import lib path

--- tools/webui.py
+++ tools/webui.py
@@ -490,7 +490,7 @@ def parse_args():
         type=Path,
         default="weights/firefly-gan-vq-fsq-8x1024-21hz-generator.pth",
     )
-    parser.add_argument("--decoder-config-name", type=str, default="gan_vq")
+    parser.add_argument("--decoder-config-name", type=str, default="firefly_gan_vq")
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--half", action="store_true")
     parser.add_argument("--compile", action="store_true")
